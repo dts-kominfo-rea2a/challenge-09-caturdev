4beb4a3718e99fe1d599a9b740a99af6890c10de
catur wicaksono <catur.wicaksono.developer@gmail.com>
2022-06-26 23:27:39 +0700

    tugas final

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ const Contact = ({ data }) => {
     return (
         <>
             <div style={{ width: '100%' }}>
-                {data.map(contact => (
+                {data.length ? data.map(contact => (
                     <div key={contact.phone}>
                         <div style={{ display: 'flex', justifyContent: 'center' }}>
                             <div style={{
@@ -23,7 +23,21 @@ const Contact = ({ data }) => {
                             </div>
                         </div>
                     </div>
-                ))
+                )) :
+                    <div style={{ display: 'flex', justifyContent: 'center' }}>
+                        <div style={{
+                            display: 'flex', backgroundColor: '#efeded', width: '30em', marginBottom: 10, borderRadius: '10px'
+                        }}>
+                            <div style={{ width: '50em' }}>
+                                <img alt="Gambar Kucing" src={data.photo} style={{ borderRadius: '100px', margin: 20 }} width="150" />
+                            </div>
+                            <div style={{ width: '50em', display: 'flex', justifyContent: 'center', flexDirection: 'column', alignContent: 'center' }}>
+                                <h6 style={{ margin: 10 }}>{data.name}</h6>
+                                <p style={{ margin: 10 }}>{data.phone}</p>
+                                <p style={{ margin: 0 }}>{data.email}</p>
+                            </div>
+                        </div>
+                    </div>
                 }
             </div >
         </>
